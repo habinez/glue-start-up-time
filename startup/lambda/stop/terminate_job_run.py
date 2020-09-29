5d071903cd5680decdbd96a88f6ee2817e52155b
habinez <habinezacc@hendrix.edu>
2020-09-28 20:02:44 -0400

    removed nodejs-npm

--- startup/lambda/stop/terminate_job_run.py
+++ startup/lambda/stop/terminate_job_run.py
@@ -9,7 +9,7 @@ logger.setLevel(logging.INFO)
 def handler(event, context):
     glue_client = client("glue")
     logger.info(json.dumps(event, default=str, indent=4))
-    run_ids = eval(event['run_ids'])
+    run_ids = event['run_ids']
     if 'job_name' in event.keys():
         try:
             response = glue_client.batch_stop_job_run(
