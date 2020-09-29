7d1194747276c4fa40cf0e2541b4c0e571e1bdc7
habinez <habinezacc@hendrix.edu>
2020-09-29 07:52:55 -0400

    added region and account in env

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
+import os
 from aws_cdk import core
+
 
 from startup.startup_stack import StartupStack
 
 app = core.App()
-StartupStack(app, "startup")
+StartupStack(app, "startup",
+             env=core.Environment(account=os.environ["CDK_DEFAULT_ACCOUNT"],
+                                  region=os.environ["CDK_DEFAULT_REGION"]
+                                  )
+             )
 
 core.Tag.add(app, "Owner", "self")
 core.Tag.add(app, "Purpose", "demo")
