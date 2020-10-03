46f6e37947155dc214a7a975c13656e0d2cd99e3
habinez <habinezacc@hendrix.edu>
2020-10-03 08:54:46 -0400

    changed the envs

--- app.py
+++ app.py
@@ -5,13 +5,11 @@ from aws_cdk import core
 
 
 from startup.startup_stack import StartupStack
-
+env=core.Environment(account=os.environ["CDK_DEFAULT_ACCOUNT"],
+                     region=os.environ["CDK_DEFAULT_REGION"]
+                    )
 app = core.App()
-StartupStack(app, "startup",
-             env=core.Environment(account=os.environ["CDK_DEFAULT_ACCOUNT"],
-                                  region=os.environ["CDK_DEFAULT_REGION"]
-                                  )
-             )
+StartupStack(app, "startup")
 
 core.Tag.add(app, "Owner", "self")
 core.Tag.add(app, "Purpose", "demo")
